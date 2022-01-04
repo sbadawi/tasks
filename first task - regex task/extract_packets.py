3646da367db19a726a6fa85381077448aa5b5635
sbadawi <sbadawi@nvidia.com>
2022-01-04 12:27:01 +0200

    version 4

--- first task - regex task/extract_packets.py
+++ first task - regex task/extract_packets.py
@@ -30,111 +30,95 @@ packets = packets_file_content.split('port ')
 packets.pop(0) # remove the first element of the list since it doesn't contain anything
 print("The number of packets : ", len(packets)) # print the number of the packets
 
-search_pattern = " (src)=([\w:]*) [\S]* (dst)=([\w:]*) [\S]* (type)=([\w]*) [\S]* (length)=([\w]*) [\S]* (nb_segs)=([\w]*) [\S]* (RSS hash)=([\w]*) [\S]* (RSS queue)=([\w]*) [[\S]* (hw ptype): ([\w ]*)]* [[\S]* (sw ptype): ([\w ]*) [\S]*]*[ \- ]* (l2_len)=([\w]*)([ \- ]* (inner_l2_len)=([\w]*))*([ \- ]* (l3_len)=([\w]*))*([ \- ]* (inner_l3_len)=([\w]*))*([ \- ]* (l4_len)=([\w]*))*[ \- ]*(Receive queue)=([\w]*)[[\n ]*(ol_flags): ([\w ]*)]*"
+search_pattern = " src=(?P<src>[\w:]*) [\S]* dst=(?P<dst>[\w:]*) [\S]* type=(?P<type>[\w]*) [\S]* length=(?P<length>[\w]*) [\S]* nb_segs=(?P<nb_segs>[\w]*) [\S]* RSS hash=(?P<rss_hash>[\w]*) [\S]* RSS queue=(?P<rss_queue>[\w]*) [[\S]* hw ptype: (?P<hw_ptype>[\w ]*)]* [[\S]* sw ptype: (?P<sw_ptype>[\w ]*) [\S]*]*[ \- ]* l2_len=(?P<l2_len>[\w]*)([ \- ]* inner_l2_len=(?P<inner_l2_len>[\w]*))*([ \- ]* l3_len=(?P<l3_len>[\w]*))*([ \- ]* inner_l3_len=(?P<inner_l3_len>[\w]*))*([ \- ]* l4_len=(?P<l4_len>[\w]*))*[ \- ]*Receive queue=(?P<receive_queue>[\w]*)[[\n ]*ol_flags: (?P<ol_flags>[\w ]*)]*"
 for packet in packets:
-    index = 1
-    packet_h = Packet()
-    packet_analysis = re.search(search_pattern, packet)
     
+    packet_analysis = re.search(search_pattern, packet)
+    packet_h = Packet()
     if(packet_analysis != None): 
 
-        if(packet_analysis.group(index) == 'src'):
-            packet_h.src = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('src')):
+            packet_h.src = packet_analysis.group('src')
         else:
             packet_h.src = None  
 
-        if(packet_analysis.group(index) == 'dst'):
-            packet_h.dst = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('dst')):
+            packet_h.dst = packet_analysis.group('dst')
         else:
             packet_h.dst = None
 
-        if(packet_analysis.group(index) == 'type'):
-            packet_h.type = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('type')):
+            packet_h.type = packet_analysis.group('type')
         else:
             packet_h.type = None
 
-        if(packet_analysis.group(index) == 'length'):
-            packet_h.length = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('length')):
+            packet_h.length = packet_analysis.group('length')
         else:
             packet_h.length = None
 
-        if(packet_analysis.group(index) == 'nb_segs'):
-            packet_h.nb_segs = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('nb_segs')):
+            packet_h.nb_segs = packet_analysis.group('nb_segs')
         else:
             packet_h.nb_segs = None
 
-        if(packet_analysis.group(index) == 'RSS hash'):
-            packet_h.rss_hash = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('rss_hash')):
+            packet_h.rss_hash = packet_analysis.group('rss_hash')
         else:
             packet_h.rss_hash = None
 
-        if(packet_analysis.group(index) == 'RSS queue'):
-            packet_h.rss_queue = packet_analysis.group(index+1)
-            index += 2 # the number of seperating groups
+        if(packet_analysis.group('rss_queue')):
+            packet_h.rss_queue = packet_analysis.group('rss_queue')
         else:
             packet_h.rss_queue = None
 
-        if(packet_analysis.group(index) == 'hw ptype'):
-            packet_h.hw_ptype = packet_analysis.group(index+1).split(' ')
+        if(packet_analysis.group('hw_ptype')):
+            packet_h.hw_ptype = packet_analysis.group('hw_ptype').split(' ')
             packet_h.hw_ptype.pop(len(packet_h.hw_ptype)-1)
-            index += 2 # the number of seperating groups
         else:
             packet_h.hw_ptype = None
 
-        if(packet_analysis.group(index) == 'sw ptype'):
-            packet_h.sw_ptype = packet_analysis.group(index+1).split(' ')
+        if(packet_analysis.group('sw_ptype')):
+            packet_h.sw_ptype = packet_analysis.group('sw_ptype').split(' ')
             packet_h.sw_ptype.pop(len(packet_h.sw_ptype)-1)
-            index += 2 # the number of seperating groups
         else:
             packet_h.sw_ptype = None
 
 
-        if(packet_analysis.group(index) == 'l2_len'):
-            packet_h.l2_len = packet_analysis.group(index+1)
+        if(packet_analysis.group('l2_len')):
+            packet_h.l2_len = packet_analysis.group('l2_len')
             
         else:
             packet_h.l2_len = None
-        index += 3 # the number of seperating groups
 
-        if(packet_analysis.group(index) == 'inner_l2_len'):
-            packet_h.inner_l2_len = packet_analysis.group(index+1)
+        if(packet_analysis.group('inner_l2_len')):
+            packet_h.inner_l2_len = packet_analysis.group('inner_l2_len')
         else:
             packet_h.inner_l2_len = None
-        index += 3 # the number of seperating groups
 
-        if(packet_analysis.group(index) == 'l3_len'):
-            packet_h.l3_len = packet_analysis.group(index+1)
+        if(packet_analysis.group('l3_len')):
+            packet_h.l3_len = packet_analysis.group('l3_len')
         else:
             packet_h.l3_len = None
-        index += 3 # the number of seperating groups
 
-        if(packet_analysis.group(index) == 'inner_l3_len'):
-            packet_h.inner_l3_len = packet_analysis.group(index+1)
+        if(packet_analysis.group('inner_l3_len')):
+            packet_h.inner_l3_len = packet_analysis.group('inner_l3_len')
         else:
             packet_h.inner_l3_len = None
-        index += 3 # the number of seperating groups
 
-        if(packet_analysis.group(index) == 'l4_len'):
-            packet_h.l4_len = packet_analysis.group(index+1)
+        if(packet_analysis.group('l4_len')):
+            packet_h.l4_len = packet_analysis.group('l4_len')
         else:
             packet_h.l4_len = None
-        index += 2 # the number of seperating groups
 
-        if(packet_analysis.group(index) == 'Receive queue'):
-            packet_h.receive_queue = packet_analysis.group(index+1)
+        if(packet_analysis.group('receive_queue')):
+            packet_h.receive_queue = packet_analysis.group('receive_queue')
         else:
             packet_h.receive_queue = None
-        index += 2 # the number of seperating groups    
 
 
-        if(packet_analysis.group(index) == 'ol_flags'):
-            packet_h.ol_flags = packet_analysis.group(index+1).split(' ')
+        if(packet_analysis.group('ol_flags')):
+            packet_h.ol_flags = packet_analysis.group('ol_flags').split(' ')
         else:
             packet_h.ol_flags = None
 
